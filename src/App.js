a359ec7f1500fc701b38770612f209e8688870fa
Pgsalbuquerque <albuquerquepgs@gmail.com>
2021-10-13 16:32:19 -0300

    visual completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import './App.css';
 
+import flecha from './assets/flechas.png'
+
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [encurtar, setEncurtar] = useState(true)
+
+  const handleSubmit = () => {
+    setEncurtar(false)
+  }
+
+  const handleCopy = () => {
+    setEncurtar(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="title">Encurtador de Links</h1>
+      {encurtar 
+      ? 
+      <>
+        <div class='ContainerEncurtar'>
+          <input 
+          class = 'inputEncurtar'
+          type="text" 
+          value={inputText} 
+          placeholder="Digite seu link aqui"
+          onChange={e => setInputText(e.target.value)}
+          />
+          <button class="buttonEncurtar" onClick={handleSubmit}>ENCURTAR</button>
+        </div>
+      </> 
+      : 
+      <>
+        <div class='ContainerEncurtado'>
+            <input 
+            class = 'inputEncurtado'
+            type="text" 
+            value={inputText} 
+            placeholder="Digite seu link aqui"
+            onChange={e => setInputText(e.target.value)}
+            />
+            <CopyToClipboard text={inputText}>
+                <button class="buttonEncurtado" onClick={handleCopy}>COPIAR</button> 
+            </CopyToClipboard>
+        </div>
+      </>
+      }
+      <div class="historyContainer">
+        <div class="history">
+          <b class="leftB">https://google.com.br</b>
+          <img style={{height: '30px'}} src={flecha}/>
+          <b class="rightB">https://whatsapp.com.br</b>
+        </div>
+        <div class="history">
+          <b class="leftB">https://facebook.com.br</b>
+          <img style={{height: '30px'}} src={flecha}/>
+          <b class="rightB">https://whatsapp.com.br</b>
+        </div>
+        <div class="history">
+          <b class="leftB">https://whatsapp.com.br</b>
+          <img style={{height: '30px'}} src={flecha}/>
+          <b class="rightB">https://whatsapp.com.br</b>
+        </div>
+      </div>
     </div>
   );
 }
